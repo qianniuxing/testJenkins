30dc4f17e5074b1181b3e9eb8900d3e7093a0c0a
niujunwei <niu13222>
2020-04-05 14:42:09 +0800

    测试

---
+++ src/main/java/com/qingniuxing/TestJenkinsApplication.java
@@ -0,0 +1,20 @@
+package com.qingniuxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName TestJenkinsApplication
+ * @Description TODO
+ * @Author niu
+ * @Date 2020/4/5
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class TestJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/qingniuxing/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.qingniuxing.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName TestController
+ * @Description TODO
+ * @Author niu
+ * @Date 2020/4/5
+ * @Version 1.0
+ **/
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @RequestMapping("/getApp")
+    public String getApp() {
+        return "测试TestJenkinsApplication";
+    }
+
+}
